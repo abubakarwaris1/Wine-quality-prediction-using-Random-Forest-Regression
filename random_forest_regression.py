12a17bd3b86de8f68721948b0cdcc97a438845b2
Muhammad Abu Bakar Waris <42335919+abubakarwaris1@users.noreply.github.com>
2020-02-15 11:56:07 +0500

    Add files via upload

---
+++ random_forest_regression.py
@@ -0,0 +1,40 @@
+# Random Forest Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('D:\\Udemy - Machine Learning\\practice\\wineQuality-random Forest\\winequality-red.csv')
+X = dataset.iloc[:, 0:11].values
+y = dataset.iloc[:, 11].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+# Fitting Random Forest Regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
+regressor.fit(X_train, y_train)
+
+# Predicting a new result
+y_pred = regressor.predict(X_test).round()
+
+
+noOfWrongPredictions=0
+wrong=y_pred-y_test
+for i in range(0,len(wrong)):
+    if(wrong[i]!=0):
+        noOfWrongPredictions=noOfWrongPredictions+1
+
+
